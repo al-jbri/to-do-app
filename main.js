20e08a28b4a88a8ca561a5475c06e9fe89f9b5a7
Ahmed Al-jbri <aljbri.dev@gmail.com>
2026-01-08 20:56:48 +0300

    adding the delete button and modifying the checkbox class

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-addTask = document.getElementById("add-task");
+const addTask = document.getElementById("add-task");
 
 addTask.onsubmit = (e) => {
   e.preventDefault();
 
-  const taskTitle = document.getElementById("title-input").value;
-  const taskStatus = document.getElementById("status-select").value;
+  const taskTitle = document.getElementById("title-input");
+  const taskStatus = document.getElementById("status-select");
   const taskContainer = document.getElementById("tasks-container");
   const taskHTML = `
-  	<input type="checkbox" />
-    <span class="task-title">${taskTitle}</span>
-    <span>${taskStatus}</span>
+  	<input type="checkbox" class="check-box" />
+    <span class="task-title">${taskTitle.value}</span>
+    <span>${taskStatus.value}</span>
+    <span class="material-symbols-outlined delete-button" aria-hidden="true">Delete</span>
 	`;
 
-  if (taskTitle.trim() === "") {
+  if (taskTitle.value.trim() === "") {
     addTask.classList.add("error");
     return;
   } else {
@@ -20,7 +21,18 @@ addTask.onsubmit = (e) => {
   }
 
   const task = document.createElement("div");
-  task.classList.add("task-item", taskStatus);
+  task.classList.add("task-item", taskStatus.value);
   task.innerHTML = taskHTML;
   taskContainer.appendChild(task);
+  taskTitle.value = "";
 };
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-button")) {
+    e.target.parentElement.remove();
+  }
+
+  if (e.target.classList.contains("check-box")) {
+    e.target.parentElement.classList.toggle("finish");
+  }
+});
